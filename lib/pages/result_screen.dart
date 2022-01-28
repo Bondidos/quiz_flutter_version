4ce1b8a818beeaa1273bc358b5381fa9123c99c5
Bondidos <gglbond@gmail.com>
2022-01-28 20:52:35 +0300

    Feature shareResult

--- lib/pages/result_screen.dart
+++ lib/pages/result_screen.dart
@@ -1,10 +1,25 @@
+import 'package:android_intent_plus/android_intent.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:platform/platform.dart';
 
 class ResultScreen extends StatelessWidget {
   final List<String> result;
 
   const ResultScreen({Key? key, required this.result}) : super(key: key);
+
+  void shareResult() {
+    final message = result.first + result.last;
+    final _intent = AndroidIntent(
+      action: 'android.intent.action.SEND',
+      type: 'text/plain',
+      arguments: {
+        'android.intent.extra.SUBJECT': 'Quiz results',
+        'android.intent.extra.TEXT' : message,
+      }
+    );
+    _intent.launch();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,12 +36,16 @@ class ResultScreen extends StatelessWidget {
               Text(result.first),
               IconButton(
                 onPressed: () {
-                 print( SystemChannels.platform);
+                  print(SystemChannels.platform);
                 },
                 icon: const Icon(Icons.restart_alt_sharp),
               ),
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  if(const LocalPlatform().isAndroid){
+                    shareResult();
+                  }
+                },
                 icon: const Icon(Icons.share),
               ),
               IconButton(
